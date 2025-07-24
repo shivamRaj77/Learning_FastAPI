c74f519d28541e3469d9e13350ed1e577ef0d6d7
SHIVAM RAJ <shivamteg007@gmail.com>
2025-07-24 10:05:03 +0530

    Added more HTTP methods

--- api1.py
+++ api1.py
@@ -1,7 +1,7 @@
 # Creating a simple inventory management system API using FastAPI
 
 import uvicorn 
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Query, HTTPException, status
 from typing import Optional
 from pydantic import BaseModel
 
@@ -22,6 +22,13 @@ class Item(BaseModel):
     # Optional makes the parameter optional
     # and (= None) sets the default to None 
 
+# without this class the update_item function can update only specific variables only
+class UpdateItem(BaseModel):
+    name: Optional[str] = None
+    price: Optional[float] = None
+    brand: Optional[str] = None
+
+
 # Endpoint/Path Parameters
 @app.get("/get-item/{item_id}")
 def get_item(item_id: int = Path(..., description="The ID of Item you want to view", gt=0, lt=10)):
@@ -33,24 +40,38 @@ def get__item(name: Optional[str]):
     for item_id in inventory:
         if inventory[item_id]['name']==name:
             return inventory[item_id]
-    return {"Data": "Not found"}
+    raise HTTPException(status_code=404, detail="Item not found!")
 
 # Request Body and POST Method
 @app.post("/create-item/{item_id}")
-def create_item(item_id: int, item: Item):
+def create_item(item_id: int, item: UpdateItem):
     if item_id in inventory:
-        return {"error": "item already in inventory"}
+        raise HTTPException(status_code=404, detail="Item already in Inventory.")
     inventory[item_id] = {"name": item.name, "price": item.price, "brand": item.brand}
     # inventory[item_id] = item
     return inventory[item_id]
 
 # PUT Method
 @app.put("/update-item/{item_id}")
-def update_item(item_id: int, item: Item):
+def update_item(item_id: int, item: UpdateItem):
     if item_id not in inventory:
-        return {"error": "Item not in Inventory"}
-    inventory[item_id].update(item)
+        raise HTTPException(status_code=404, detail="Item not found!")
+    
+    if item.name != None:
+        inventory[item_id]['name'] = item.name
+    if item.price != None:
+        inventory[item_id]['price'] = item.price
+    if item.brand != None:
+        inventory[item_id]['brand'] = item.brand
+
     return inventory[item_id]
+
+@app.delete("/delete-item")
+def delete_item(item_id: int = Query(..., description="The ID of the item to delete", gt=0, lt=10)):
+    if item_id not in inventory:
+        raise HTTPException(status_code=404, detail="Item not found!")
+    del inventory[item_id]
+    return {"Success": "Item removed"}
     
 if __name__ == "__main__":
     uvicorn.run(app, host='127.0.0.1', port=8000)
